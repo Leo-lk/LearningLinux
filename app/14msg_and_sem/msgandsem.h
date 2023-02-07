020c0ac8bd38d4bbc1ee3ed78a4c6e61dc3eaba9
eden.lu <eden.lu@quectel.xom>
2023-02-07 03:37:58 -0800

    tmp

---
+++ app/14msg_and_sem/msgandsem.h
@@ -0,0 +1,8 @@
+#ifndef __MSGANDSEM_H__
+#define __MSGANDSEM_H__
+
+#define KEY 1234
+
+
+
+#endif
--- app/8pipe/pipe.c
+++ app/8pipe/pipe.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
     /* 初始化pipe */
-    if (pipe(pipefd) == -1)
+    if (pipe(pipefd) < 0)
     {
         printf("pipe failed\n");
         exit(EXIT_FAILURE);
