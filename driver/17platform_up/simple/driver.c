35361c5094a6c8fecfce1548326f39419d60e3c3
eden <1458277939@qq.com>
2024-05-02 19:14:08 +0800

    optimize platform driver

--- driver/17platform_up/simple/driver.c
+++ driver/17platform_up/simple/driver.c
@@ -4,20 +4,41 @@
 
 MODULE_LICENSE("GPL");
 
+struct local_dev_t{
+    struct platform_device *pdev;
+};
+
 /* 匹配成功后调用 */
 int hello_probe(struct platform_device *pdev)
 {
+    int ret = 0;
+    struct local_dev_t *local_dev;
+    
     printk("driver match ok \n");
     printk("mem = 0x%x\n", pdev->resource[0].start);
     printk("irq = %d\n", pdev->resource[1].start);
     /* 申请内存 */
     /* 注册中断 */
+
+    local_dev = devm_kzalloc(&pdev->dev, sizeof(*local_dev), GFP_KERNEL);
+    if (!local_dev) {
+        ret = ENOMEM;
+        return ret;
+    }
+
+
+    /* 把local_dev放到pdev->dev.drvdata中 */
+    platform_set_drvdata(pdev, &local_dev);
+
+
     return 0;
 }
 /* 驱动或设备一方卸载后调用 */
 int hello_remove(struct platform_device *pdev)
 {
+    struct local_dev_t *local_dev = pdev->dev.driver_data;
     printk("driver hello remove \n");
+    devm_kfree(&pdev->dev, local_dev);
     return 0;
 }
 
